8d145d62bf4b505e952ec09419978d22d628563a
Kris <564225149@qq.com>
2017-06-11 12:24:22 +0800

    webmagic test

---
+++ src/main/java/com/kris/webmagic/SzuOlddriverTest.java
@@ -0,0 +1,31 @@
+package com.kris.webmagic;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+/**
+ * Created by Kris on 2017/6/11.
+ */
+public class SzuOlddriverTest implements PageProcessor{
+
+    private Site site = Site.me();
+
+    @Override
+    public void process(Page page) {
+        System.out.println(page.getHtml().xpath("//div[@class='movie-info']/span/text()").all());
+    }
+
+    @Override
+    public Site getSite() {
+        return site;
+    }
+
+    public static void main(String[] args) {
+        Spider.create(new SzuOlddriverTest())
+                .addUrl("https://www.szuolddriver.com/rank/imdbdouban")
+                .thread(1)
+                .run();
+    }
+}
