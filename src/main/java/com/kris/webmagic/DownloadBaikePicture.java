70873f230ecb3811809d203bc170f99c6d0ff0b9
Kris <564225149@qq.com>
2017-06-15 00:08:47 +0800

    爬百度百科图片

--- src/main/java/com/kris/webmagic/DownloadBaikePicture.java
+++ src/main/java/com/kris/webmagic/DownloadBaikePicture.java
@@ -1,10 +1,14 @@
 package com.kris.webmagic;
 
-import com.alibaba.fastjson.JSONObject;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
 
 /**
  * Created by Kairou Zeng on 2017/6/14.
@@ -15,8 +19,28 @@ public class DownloadBaikePicture implements PageProcessor{
 
     @Override
     public void process(Page page) {
+        String urlSring = page.getHtml().xpath("//div[@class='summary-pic']//img/@src").get();
+        try {
+            URL url = new URL(urlSring);
+            DataInputStream dataInputStream = new DataInputStream(url.openStream());
+            String name = page.getHtml().xpath("//dd[@class='lemmaWgt-lemmaTitle-title']/h1/text()").get() + ".png";
+            File file = new File("F:\\pic\\");
+            if(!file.isDirectory()){
+                file.mkdir();
+            }
+            FileOutputStream fileOutputStream = new FileOutputStream("F:\\pic\\" + name);
+            byte[] buffer = new byte[1024];
+            int length;
+            while((length = dataInputStream.read(buffer)) > 0){
+                fileOutputStream.write(buffer, 0, length);
+            }
+            dataInputStream.close();
+            fileOutputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        System.out.println(page.getHtml().xpath("//div[@class='summary-pic']//img/@src").get());
+        System.out.println();
     }
 
     @Override
